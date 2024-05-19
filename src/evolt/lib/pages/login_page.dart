85ba06275aad890054346bc3d39af305417749c2
Trisinus123 <trisinusgl@gmail.com>
2024-05-19 12:34:41 +0700

    Halaman Login

--- src/evolt/lib/pages/login_page.dart
+++ src/evolt/lib/pages/login_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 void main() {
-  runApp(const LoginPage());
+  runApp(const MyApp());
 }
 
-=======
->>>>>>> f753c2e0199f191071fb38dcf36120348068773f
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const LoginPage(),
+      routes: {
+        '/home': (context) => const HomePage(), 
+      },
+    );
+  }
+}
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -20,131 +31,10 @@ class _LoginPageState extends State<LoginPage> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return MaterialApp(
-<<<<<<< HEAD
-        home: Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: const Color(0xFFEFEEEC),
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              height: screenHeight * 0.9,
-              width: screenWidth,
-              decoration: BoxDecoration(
-                color: const Color(0xFF322C39),
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(80),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.4),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: screenHeight * 0.24),
-                    Image.asset(
-                      'assets/evolt-logo.png',
-                      width: 276,
-                      height: 276,
-                    ),
-                    const SizedBox(height: 40),
-                    SizedBox(
-                      width: screenWidth * 0.71,
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            hintText: 'Username',
-                            filled: true,
-                            fillColor: Color(0xFF322C39),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                width: 3.0,
-                                color: Color(0xFFC92C6C),
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                  width: 2, color: Color(0xFF609FA1)),
-                            ),
-                            hintStyle: TextStyle(color: Color(0xFFEFEEEC))),
-                        style: TextStyle(color: Color(0xFFEFEEEC)),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: screenWidth * 0.71,
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            hintText: 'Password',
-                            filled: true,
-                            fillColor: Color(0xFF322C39),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                  width: 3, color: Color(0xFFC92C6C)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                  width: 2, color: Color(0xFF609FA1)),
-                            ),
-                            hintStyle: TextStyle(color: Color(0xFFEFEEEC))),
-                        style: TextStyle(color: Color(0xFFEFEEEC)),
-                        obscureText: true,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                      width: screenWidth * 0.37,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              const EdgeInsets.all(16)),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              const Color(0xFF609FA1)),
-                          shadowColor: MaterialStateProperty.all<Color>(
-                              Colors.black.withOpacity(0.8)),
-                          elevation: MaterialStateProperty.all(5),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/home');
-                        },
-                        child: const Text(
-                          'Masuk',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          )
-        ],
-=======
-        home: SingleChildScrollView(
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: Stack(
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Stack(
           children: <Widget>[
             Container(
               color: const Color(0xFFEFEEEC),
@@ -261,8 +151,19 @@ class _LoginPageState extends State<LoginPage> {
             )
           ],
         ),
->>>>>>> f753c2e0199f191071fb38dcf36120348068773f
       ),
-    ));
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: const Center(child: Text('Welcome to the Home Page')),
+    );
   }
 }
