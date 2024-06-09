a894529db4b02a6f06084b4e7ea11f8e6b1f2e65
Trisinus123 <trisinusgl@gmail.com>
2024-06-09 16:26:52 +0700

    Menambahkan Fitur Login

--- src/evolt/lib/pages/login_page.dart
+++ src/evolt/lib/pages/login_page.dart
@@ -133,110 +133,102 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ],
               ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/evolt-logo.png',
-                        width: 276,
-                        height: 276,
-                      ),
-                      const SizedBox(height: 40),
-                      SizedBox(
-                        width: screenWidth * 0.71,
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Username',
-                            filled: true,
-                            fillColor: Color(0xFF322C39),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                width: 3.0,
-                                color: Color(0xFFC92C6C),
-                              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: screenHeight * 0.14),
+                    Image.asset(
+                      'assets/evolt-logo.png',
+                      width: 276,
+                      height: 276,
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: screenWidth * 0.71,
+                      child: TextField(
+                        controller: _usernameController,
+                        decoration: const InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          fillColor: Color(0xFF322C39),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(
+                              width: 3.0,
+                              color: Color(0xFFC92C6C),
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                width: 2,
-                                color: Color(0xFF609FA1),
-                              ),
-                            ),
-                            hintStyle: TextStyle(color: Color(0xFFEFEEEC)),
                           ),
-                          style: TextStyle(color: Color(0xFFEFEEEC)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: Color(0xFF609FA1),
+                            ),
+                          ),
+                          hintStyle: TextStyle(color: Color(0xFFEFEEEC)),
+                        ),
+                        style: const TextStyle(color: Color(0xFFEFEEEC)),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: screenWidth * 0.71,
+                      child: TextField(
+                        controller: _pinController,
+                        decoration: const InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          fillColor: Color(0xFF322C39),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(
+                              width: 3,
+                              color: Color(0xFFC92C6C),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: Color(0xFF609FA1),
+                            ),
+                          ),
+                          hintStyle: TextStyle(color: Color(0xFFEFEEEC)),
                         ),
                         style: const TextStyle(color: Color(0xFFEFEEEC)),
                         obscureText: true,
                       ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: screenWidth * 0.71,
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            filled: true,
-                            fillColor: Color(0xFF322C39),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                width: 3,
-                                color: Color(0xFFC92C6C),
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)),
-                              borderSide: BorderSide(
-                                width: 2,
-                                color: Color(0xFF609FA1),
-                              ),
-                            ),
-                            hintStyle: TextStyle(color: Color(0xFFEFEEEC)),
-                          ),
-                          style: TextStyle(color: Color(0xFFEFEEEC)),
-                          obscureText: true,
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: screenWidth * 0.37,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(16)),
+                          backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF609FA1)),
+                          shadowColor: MaterialStateProperty.all<Color>(Colors.black.withOpacity(0.8)),
+                          elevation: MaterialStateProperty.all(5),
                         ),
-                      ),
-                      const SizedBox(height: 15),
-                      SizedBox(
-                        width: screenWidth * 0.37,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                                const EdgeInsets.all(16)),
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                const Color(0xFF609FA1)),
-                            shadowColor: MaterialStateProperty.all<Color>(
-                                Colors.black.withOpacity(0.8)),
-                            elevation: MaterialStateProperty.all(5),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/home');
-                          },
-                          child: const Text(
-                            'Masuk',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        onPressed: () {
+                          final username = _usernameController.text;
+                          final pin = _pinController.text;
+                          login(username, pin);
+                        },
+                        child: const Text(
+                          'Masuk',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
+          )
         ],
       ),
     );
